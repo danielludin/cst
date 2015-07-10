9d45306882fe4d881955c17654b858ae5b7323c5
danielludin <ludin@swissonline.ch>
2015-07-10 13:54:26 +0200

    - clean up (removed debug statements, added copyright header, readme and epl)

--- ch.gpb.elexis.cst/src/ch/gpb/elexis/cst/preferences/CstPreference.java
+++ ch.gpb.elexis.cst/src/ch/gpb/elexis/cst/preferences/CstPreference.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2015, Daniel Ludin
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Daniel Ludin (ludin@hispeed.ch) - initial implementation
+ *******************************************************************************/
 /**
  * @author daniel ludin ludin@swissonline.ch
  * 27.06.2015
--- ch.gpb.elexis.cst/src/ch/gpb/elexis/cst/test/VorwertCanvas2.java
+++ ch.gpb.elexis.cst/src/ch/gpb/elexis/cst/test/VorwertCanvas2.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2015, Daniel Ludin
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Daniel Ludin (ludin@hispeed.ch) - initial implementation
+ *******************************************************************************/
 package ch.gpb.elexis.cst.test;
 
 import java.text.SimpleDateFormat;
